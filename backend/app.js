69fb8be48b551f3c654c6f9958b01f940d27b4ad
Jason <jason.b.harris1@gmail.com>
2021-11-30 16:36:25 -0600

    add gitignore

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+require("dotenv").config();
+
+const api = process.env.API_URL;
+
+app.get("/api/products", (req, res) => {
   res.send("hello api!");
 });
 
-app.listen(port, () => console.log(`Server is now running on ${port}`));
+app.listen(port, () => {
+  console.log(api);
+  console.log(`Server is now running on ${port}`);
+});
